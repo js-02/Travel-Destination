59cd8edc3385e01fb11d08819b923d84e9adfcd4
samah anwer alsatary <alsatary.samah@yahoo.com>
2022-05-13 00:03:24 +0300

    update

--- app/src/components/TourDetails/TourDetails.js
+++ app/src/components/TourDetails/TourDetails.js
@@ -1,14 +1,47 @@
 
 import { Routes, Route } from "react-router-dom";
 import Tours from "../tours/Tours";
-export default function TourDetails()
-{
-return(
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import Header from "../header/Header";
 
+export default function TourDetails(props)
+{
+    let { id } = useParams();
+
+    const [isClicked, setIsClicked] = useState(false);
+    function clicking(event) {
+        event.preventDefault();
+        setIsClicked(!isClicked);
+      }
+      let tourData = props.data.filter(element=> element.id === id) ;
+      console.log("tour"+tourData[0].id);
+return(
+    
     <>
-   
-{/* <Route path="/city" element={<Tours />} />  */}
+    <Header />
+    <p>{tourData[0].name}</p>
+            <img src={tourData[0].image} ></img>
+   <p>
+                  {tourData[0].info.substring(0, 100)}
+
+                  {isClicked ? (
+                    <p>
+                      {tourData[0].info.slice(101)}
+                      <a href="" onClick={clicking}>
+                        {isClicked ? "see less" : "see more"}
+                      </a>
+                    </p>
+                  ) : (
+                    <a href="" onClick={clicking}>
+                      {isClicked ? "see less" : "see more"}
+                    </a>
+                  )}
+                </p>
+            
+
     </>
 );
 
 }
+
